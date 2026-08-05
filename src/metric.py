fdec6d96a59cfe8498708ed66fe5fe630308f067
whizikem <wisdomikem@gmail.com>
2026-08-05 20:43:10 +0100

    forecasting metric

--- src/metric.py
+++ src/metric.py
@@ -2,6 +2,7 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
+# Metrics for evaluating forecasting model performance
 def print_metrics(y_true, y_pred, label):
     mask = y_true != 0
     mape = np.mean(np.abs((y_true[mask] - y_pred[mask]) / y_true[mask])) * 100
